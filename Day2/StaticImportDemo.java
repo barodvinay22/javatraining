52f6ed0e065e6cf4669af13687779c04f2d12990
Vinay Barod <vbarod@mobiquityinc.com>
2025-09-11 05:07:07 +0530

    Day2 Code changes added

---
+++ Day2/StaticImportDemo.java
@@ -0,0 +1,11 @@
+import static java.lang.System.out;
+
+public class StaticImportDemo {
+    public static void main(String[] args) {
+        out.println("Hello World"); // out is an object of printstream class
+        out.println("Hello World");//
+        out.println("Hello World");
+        out.println("Hello World");
+        out.println("Hello World");
+    }
+}
