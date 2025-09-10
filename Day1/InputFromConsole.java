4a87a5b0cd9e329e6e1be73ab86e7af1f8b4f720
Vinay Barod <vbarod@mobiquityinc.com>
2025-09-11 05:19:35 +0530

    Input from Console

--- Day1/InputFromConsole.java
+++ Day1/InputFromConsole.java
@@ -1,4 +1,13 @@
 package Day1;
 
+import java.util.Scanner;
 public class InputFromConsole {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);//in is an object of Input Stream class
+        System.out.print("Enter a number: ");
+        int a = input.nextInt();
+        System.out.print("Enter another number: ");
+        int b = input.nextInt();
+        System.out.print("Sum=: "+ (a+b));
+    }
 }
