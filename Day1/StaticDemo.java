d7bcb405815871055bef18b6e7db6413273adb4a
Vinay Barod <vbarod@mobiquityinc.com>
2025-09-12 04:49:00 +0530

    Wrapper ClassDemo added

--- Day1/StaticDemo.java
+++ Day1/StaticDemo.java
@@ -4,6 +4,9 @@ public class StaticDemo {
     static{
         System.out.println("Inside Static");
     }
+    {
+        System.out.println("Instance Block");
+    }
 
     public static void main(String... a){
         System.out.println("Inside Main");
