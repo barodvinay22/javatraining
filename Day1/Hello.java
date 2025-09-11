41a3210eefeb052ea38a1e72ac71e63d559edeb9
barodvinay22 <vinayb1@hexaware.com>
2025-09-11 05:32:46 +0530

    Revert "StaticDemo Eample added"

--- Day1/Hello.java
+++ Day1/Hello.java
@@ -1,6 +1,6 @@
 public class Hello{
     public static void main(String[] args){
         System.out.println("Hello World");
-        System.out.println("Updated in main ");
+        System.out.prinln("Updated in main ")
     }
 }
