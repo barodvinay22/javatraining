4f8848af986e7966d0ef073ec9d71b872313ec4e
Vinay Barod <vbarod@mobiquityinc.com>
2025-09-13 04:40:55 +0530

    Runtime polyphorims

--- Day2/DoctorMain.java
+++ Day2/DoctorMain.java
@@ -1,6 +1,8 @@
 public class DoctorMain {
     public static void main(String[] args) {
-        Doctor doctor=new PadeatricDoctor("Nakul",10);
+        Doctor doctor=new PadeatricDoctor("Nakul",10);//Reference is doctor and Object is New Padeatric
+        System.out.println(doctor);
+        doctor = new Doctor("john");
         System.out.println(doctor);
     }
 }
