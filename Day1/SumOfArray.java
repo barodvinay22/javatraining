2df877bbafba14306827179ac24d7697bf41a732
kklearnsAI <keshavkhalap@yahoo.com>
2025-09-16 22:00:13 -0400

    Assignment-Sep 16 2025

---
+++ Day1/SumOfArray.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class SumOfArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] numbers = new int[10];
+        int sum = 0;
+
+        // Input 10 numbers into the array
+        System.out.println();
+        System.out.println("Enter 10 numbers to sum:");
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = sc.nextInt();
+            sum += numbers[i]; // Add each number to sum
+        }
+
+        // Display the sum of input 10 numbers
+        System.out.println();
+        System.out.println("The sum of the 10 numbers is: " + sum);
+
+        sc.close();
+    }
+}
---
+++ Day1/SwapWithThirdVar.java
@@ -0,0 +1,28 @@
+public class SwapWithThirdVar {
+    public static void main(String[] args) {
+        // Check if two arguments are provided
+        if (args.length < 2) {
+            System.out.println("Please provide two numbers as arguments.");
+            return;
+        }
+
+        // Take input from method arguments
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+
+        System.out.println();
+        System.out.println("Before swapping:");
+        System.out.println("First number : " + a);
+        System.out.println("Second number: " + b);
+
+        // Swap using a third variable
+        int temp = a;
+        a = b;
+        b = temp;
+
+        System.out.println();
+        System.out.println("After swapping:");
+        System.out.println("First number : " + a);
+        System.out.println("Second number: " + b);
+    }
+}
