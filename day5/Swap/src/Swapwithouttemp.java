2196a627f19416a1736362381422fd5f2f9aa3e9
Prakash Parker <1000027901@hexaware.com>
2025-09-17 18:54:00 -0400

    assignment-prakash

---
+++ day5/Swap/src/Swapwithouttemp.java
@@ -0,0 +1,13 @@
+public class Swapwithouttemp {
+    public static void main(String[] args) {
+        int a = 10, b = 20;
+
+        System.out.println("Before Swap: a = " + a + ", b = " + b);
+
+        a = a + b; // a = 30
+        b = a - b; // b = 10
+        a = a - b; // a = 20
+
+        System.out.println("After Swap: a = " + a + ", b = " + b);
+    }
+}
