fadda7b0f739c3d74b325d31ce2246ed778cddfe
kklearnsAI <keshavkhalap@yahoo.com>
2025-09-22 13:52:46 -0400

    Java Assignment - Sep 17 - Update

--- AssignmentSep17/StudentDetails.java
+++ AssignmentSep17/StudentDetails.java
@@ -1,6 +1,6 @@
 package AssignmentSep17;
 public class StudentDetails {
-    // Student Field Details
+    // Fields
     int rollNumber;
     String name;
     int marks;
@@ -17,5 +17,23 @@ public class StudentDetails {
         System.out.println("Roll Number: " + rollNumber);
         System.out.println("Name: " + name);
         System.out.println("Marks: " + marks);
+    }
+
+    // Main method to test the StudentDetails class
+    public static void main(String[] args) {
+        // Creating student objects with different arguments
+        StudentDetails student1 = new StudentDetails(101, "Name 1", 84);
+        StudentDetails student2 = new StudentDetails(102, "Name 2", 71);
+        StudentDetails student3 = new StudentDetails(103, "Name 3", 90);
+
+        // Displaying student details
+        System.out.println("Student 1 Details:");
+        student1.displayDetails();
+
+        System.out.println("\nStudent 2 Details:");
+        student2.displayDetails();
+
+        System.out.println("\nStudent 3 Details:");
+        student3.displayDetails();
     }
 }
